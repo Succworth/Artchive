4114ead318eec2b73b791406dd1ae4c0ca2dbdf9
Conner Giordano <connergiordano@gmail.com>
2022-04-14 14:16:06 -0400

    Finished outline for question 6

--- Question6.php
+++ Question6.php
@@ -8,6 +8,49 @@
   <body>
     <h1>What is the average age of artists of each decade when they created artwork?</h1>
     <hr>
+    <h2>Please select the era you wish to see</h2>
+    <br>
+    <br>
+    <h3>Average Age Globally</h3>
+    <a href="Question6GlobalAverage-220314-021018.html">
+      <input type=button value="Earliest Recorded - Present">
+    </a>
+    <hr>
+    <h3>Average Age by Country</h3>
+    <a href="Question6CountryAveragePart1-220314-133913.html">
+      <input type=button value="Earliest Recorded - 1631">
+    </a>
+    <br>
+    <br>
+    <a href="Question6CountryAveragePart2-220314-133933.html">
+      <input type=button value="1631 - 1760">
+    </a>
+    <br>
+    <br>
+    <a href="Question6CountryAveragePart3-220314-133940.html">
+      <input type=button value="1760 - 1845">
+    </a>
+    <br>
+    <br>
+    <a href="Question6CountryAveragePart4-220314-133946.html">
+      <input type=button value="1845 - 1906">
+    </a>
+    <br>
+    <br>
+    <a href="Question6CountryAveragePart5-220314-133950.html">
+      <input type=button value="1906 - 1938">
+    </a>
+    <br>
+    <br>
+    <a href="Question6CountryAveragePart6-220314-133954.html">
+      <input type=button value="1938 - 1973">
+    </a>
+    <br>
+    <br>
+    <a href="Question6CountryAveragePart7-220314-133958.html">
+      <input type=button value="1973 - Present">
+    </a>
+    <br>
     <br>
     <a href = "HomePage.php">
       <input type = button value = Home>
