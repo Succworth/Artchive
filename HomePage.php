cde485cf5475408e71054b378e83590ff5993811
Conner Giordano <connergiordano@gmail.com>
2022-04-16 20:09:33 -0400

    Added functionality to see tuple counts

--- HomePage.php
+++ HomePage.php
@@ -6,6 +6,11 @@
   </head>
   <center>
   <body>
+    <?php $username = "c.giordano";                  // Use your username
+    $password2 = "zGneVdKkuq9zIZQJAWxQw1SB";             // and your password
+    $database = "oracle.cise.ufl.edu/orcl";   // and the connect string to connect to your database
+    $c = oci_connect($username, $password2, $database);
+    ?>
     <h1>Welcome to the Artchive!</h1>
     <hr>
     <a href="Question1.php">
@@ -38,6 +43,85 @@
     </a>
     <br>
     <br>
+    <input type="button" onclick="countArtistsTuples()" value = "How many tuples are there in the table Artists?">
+    <?php
+    $query = "SELECT count(*) FROM Artists";
+    $stid = oci_parse($c, $query);
+    $r = oci_execute($stid);
+    while ($row = oci_fetch_array($stid, OCI_ASSOC+OCI_RETURN_NULLS)) {
+      foreach ($row as $item) {
+          $artist = $item;
+      }
+    }
+    oci_free_statement($stid);
+     ?>
+
+     <br>
+     <br>
+     <input type="button" onclick="countArtworksTuples()" value = "How many tuples are there in the table Artworks?">
+     <?php
+     $query = "SELECT count(*) FROM Artworks";
+     $stid = oci_parse($c, $query);
+     $r = oci_execute($stid);
+     while ($row = oci_fetch_array($stid, OCI_ASSOC+OCI_RETURN_NULLS)) {
+       foreach ($row as $item) {
+           $artwork = $item;
+       }
+     }
+     oci_free_statement($stid);
+      ?>
+
+      <br>
+      <br>
+      <input type="button" onclick="countMakeTuples()" value = "How many tuples are there in the table Make?">
+      <?php
+      $query = "SELECT count(*) FROM Make";
+      $stid = oci_parse($c, $query);
+      $r = oci_execute($stid);
+      while ($row = oci_fetch_array($stid, OCI_ASSOC+OCI_RETURN_NULLS)) {
+        foreach ($row as $item) {
+            $make = $item;
+        }
+      }
+      oci_free_statement($stid);
+       ?>
+
+    <script>
+      function countArtistsTuples() {
+        var num = '<?php echo $artist; ?>';
+        alert(num);
+      }
+      function countArtworksTuples() {
+        var num = '<?php echo $artwork; ?>';
+        alert(num);
+      }
+      function countMakeTuples() {
+        var num = '<?php echo $make; ?>';
+        alert(num);
+      }
+      function test() {
+        alert("Here!");
+      }
+    </script>
+
+    <?php
+    /*
+    $query = "SELECT count(*) FROM Artists";
+    $stid = oci_parse($c, $query);
+    $r = oci_execute($stid);
+    print "<table border='1'>\n";
+    while ($row = oci_fetch_array($stid, OCI_ASSOC+OCI_RETURN_NULLS)) {
+      print "<tr>\n";
+      foreach ($row as $item) {
+          print "    <td>" . ($item !== null ? htmlentities($item, ENT_QUOTES) : "&nbsp;") . "</td>\n";
+          $num = $item;
+      }
+      print "</tr>\n";
+    }
+    print "</table>\n";
+    echo $num;
+    oci_free_statement($stid); */
+     ?>
   </body>
   </center>
 </html>
